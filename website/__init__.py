7f9167d52bb136515b7ec22af2ba012300a1209b
James-Yu-2007 <yujames2007@gmail.com>
2022-12-28 13:49:19 -0800

    deploy script

--- website/__init__.py
+++ website/__init__.py
@@ -2,15 +2,16 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
-
+DB_PATH = os.environ['DB_PATH']
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_PATH}'
     db.init_app(app)
 
     from .views import views
@@ -35,7 +36,7 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_NAME):
+    if not path.exists('website/' + DB_PATH):
         with app.app_context():
             db.create_all()
         print('Created Database!')
