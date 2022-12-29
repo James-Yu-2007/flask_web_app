beecd68371742874447e2edb98f289b9f291f3ac
James-Yu-2007 <yujames2007@gmail.com>
2022-12-28 17:27:06 -0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,22 +1,24 @@
 mkdir /database && mkdir /website && mkdir /var/log/website
 if [ $? -ne 0 ]
-then 
+then
     echo "FATAL: directories created unsuccessfully"
     exit 1
 fi
 
-apt install python3-pip && pip3 install flask && pip3 install flask-login && pip3 install flask-sqlalchemy
+yes | apt install python3-pip && pip3 install flask && pip3 install flask-logi>
 if [ $? -ne 0 ]
-then 
+then
     echo "FATAL: packages downloaded unsuccessfully"
     exit 2
 fi
 
 export DB_PATH=/database/database.db
 
-wget -p /website https://github.com/James-Yu-2007/flask_web_app/archive/refs/tags/v1.0.zip && unzip /website/v1.0.zip && rm /website/v1.0.zip && nohup python3 /website/flask_web_app-1.0/main.py > /var/log/website/console.log
+wget -p /website https://github.com/James-Yu-2007/flask_web_app/archive/refs/t>
 if [ $? -ne 0 ]
-then 
+then
     echo "FATAL: deployed unsuccessfully"
     exit 3
+else
+    echo SUCCESS
 fi
