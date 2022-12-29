3df16a3e58c658925b37bba76b8dcbaff4bace36
James-Yu-2007 <yujames2007@gmail.com>
2022-12-28 18:21:37 -0800

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -38,10 +38,4 @@ fi
 export DB_PATH=/database/database.db
 
 wget -p /website https://github.com/James-Yu-2007/flask_web_app/archive/refs/tags/v1.0.zip && unzip /website/v1.0.zip && rm /website/v1.0.zip && nohup python3 /website/flask_web_app-1.0/main.py > /var/log/website/console.log
-if [ $? -ne 0 ]
-then
-    echo "FATAL: deployed unsuccessfully"
-    exit 3
-else
-    echo SUCCESS
-fi
+echo SUCCESS
