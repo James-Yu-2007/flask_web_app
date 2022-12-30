58c32be9087f1ead47c064788efb58c7c469fff8
James-Yu-2007 <yujames2007@gmail.com>
2022-12-30 14:46:46 -0800

    superlinter

--- website/__init__.py
+++ website/__init__.py
@@ -36,7 +36,8 @@ def create_app():
 
 
 def create_database(app):
-    if not path.exists('website/' + DB_PATH):
+# add ("website/" +) back to the line below when finished with pipeline 
+    if not path.exists(DB_PATH):
         with app.app_context():
             db.create_all()
         print('Created Database!')
