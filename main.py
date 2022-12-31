2838e320cbd434837c81765f232d715014ce4591
James-Yu-2007 <yujames2007@gmail.com>
2022-12-31 14:49:12 -0800

    superlinter

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from website import create_app
-import socket
 import os
 
 app = create_app()
 
 LOCAL_IP = os.environ['LOCAL_IP']
 
+LOCAL_IP = LOCAL_IP.split()
+
 if __name__ == '__main__':
-    app.run(host=LOCAL_IP, debug=True)
+    app.run(host=LOCAL_IP[0], debug=True)
