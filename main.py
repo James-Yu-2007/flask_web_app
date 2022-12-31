4306fc5e2b5b8732f38046fd6d1a79f5fad826e3
James-Yu-2007 <yujames2007@gmail.com>
2022-12-31 15:34:50 -0800

    superlinter

--- main.py
+++ main.py
@@ -7,17 +7,5 @@ LOCAL_IP = os.environ['LOCAL_IP']
 
 LOCAL_IP = LOCAL_IP.split()
 
-listeningOrNot = os.environ['listeningOrNot']
-
-listeningOrNot = listeningOrNot.split()
-
-def listen():
-    for i in listeningOrNot:
-        if i == "LISTEN":
-            return True
-
 if __name__ == '__main__':
     app.run(host=LOCAL_IP[0], debug=True)
-
-if listen():
-    print("the 5000 port is listening")
